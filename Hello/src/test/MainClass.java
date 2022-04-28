ecbb59989c7139014c3223a6a77b547e3a6b9adb
cothe <cothe017@gmail.com>
2022-04-28 23:33:28 +0900

    Hello 프로젝트

---
+++ Hello/src/test/MainClass.java
@@ -0,0 +1,19 @@
+package test;
+
+public class MainClass {
+	   //run(실행) 했을대 실행의 흐름이 시작 되는 main 메소드(application이 실행이 되려면 시작점이 있어야함)
+	   //이 main 메소드는 run 했을때, 실행의 흐름이 시작되는 특별한 메소드
+	   //args 를 제외한 나머지는 아무것도 바꾸면 안됨(바꾸면 제 역할을 못함)
+	   //public static void main2(String[] 에러남
+       public static void main(String[] args) {
+       //run하면 여기서 시작이 되는것   
+    	   System.out.println("Hello, Wordl!");
+    	   
+    	   String str = "kimgura";
+    	   
+    	   int num1=10;
+    	   
+    	   boolean isRun=true;
+    	   
+       }
+}
