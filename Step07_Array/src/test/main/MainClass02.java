97ebbd333149b3df0584e289295f555b59d385f6
cothe <cothe017@gmail.com>
2022-04-29 22:20:45 +0900

    07 Array MainClass02 edit

--- Step07_Array/src/test/main/MainClass02.java
+++ Step07_Array/src/test/main/MainClass02.java
@@ -3,7 +3,7 @@ package test.main;
 public class MainClass02 {
     public static void main(String[] args) {
 		// 0으로 초기화된 방 3개짜리 int[] 객체 만들어서 참조값을 지역 변수 nums 에 담기
-    	int[] nums= { 0, 0, 0};
+    	int[] nums= { 0 , 0 , 0 };
     	nums[0]=10;
     	nums[1]=20;
     	nums[2]=30;
