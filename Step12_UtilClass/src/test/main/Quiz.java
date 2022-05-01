92637d6490ffa03732db5b82497658e91acf5948
cothe <cothe017@gmail.com>
2022-05-02 00:30:06 +0900

    Quiz

---
+++ Step12_UtilClass/src/test/main/Quiz.java
@@ -0,0 +1,15 @@
+package test.main;
+/*
+ *  run 을 누르면 로또번호 6 개를 랜덤하게 추력하는 코드를 작성해 보세요.
+ * 
+ *  1~45 사이의 숫자 중에 6개가 나와야 하며
+ *  중복되는 숫자가 있으면 안되고
+ *  작은 숫자부터 큰숫자까지 정렬까지 해서 출력해 보세요.
+ * 
+ * 
+ */
+public class Quiz {
+    public static void main(String[] args) {
+		
+	}
+}
