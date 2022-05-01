8b1dc0d1e7ce8e053d265f595012833587bdf307
cothe <cothe017@gmail.com>
2022-05-01 23:59:03 +0900

    Step09 MainClass03

--- Step09_AbstractClass/src/test/main/MainClass03.java
+++ Step09_AbstractClass/src/test/main/MainClass03.java
@@ -15,11 +15,12 @@ public class MainClass03 {
     	Zoo z=new Zoo();
 		Monkey m1=z.getMonkey();
         m1.say();
+        
         // Zoo 클래스에 있는 getTiger() 메소드를 호출해서
         // 리턴되는 값을 t1 이라는 지역 변수에 담아 보세요.
-
         Tiger t1=z.getTiger();
         t1.say();
+        
         // 메소드 안에도 클래스를 정의 할수 있다. (메소드 안에서만 쓸수 있는 클래스)
         class Gura{
         	public void say() {
